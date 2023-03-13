5fd506fe25bd24117147f52feb52c75aa9cde76b
mbmcmullen27 <mbmcmullen27@gmail.com>
2023-03-13 18:53:47 -0500

    align with smoke tests

--- api-server.js
+++ api-server.js
@@ -17,8 +17,7 @@ app.get('/employees', function(req, res) {
 })
 
 app.post('/employees', function(req, res) {
-  console.log(req.body)
-  if (req.body.name) {
+  if (req.body.name !== undefined) {
     let employee = {
       name: req.body.name,
       github: req.body.github,
@@ -26,6 +25,10 @@ app.post('/employees', function(req, res) {
     }
   
     data.employees.push(employee)
+    console.log(`User ${employee.id} created:`)
+    for(key of Object.keys(req.body)) {
+      console.log(`  ${key}: ${employee[key]}`)
+    }
     res.send(employee)
   } else {
     res.status(400).send('Missing employee name')
@@ -33,22 +36,43 @@ app.post('/employees', function(req, res) {
 })
 
 app.get('/employees/:id', function(req, res) {
-  res.send(data.employees.filter(e=>e.id == req.params.id)[0])
+  let employee = data.employees.filter(e=>e.id == req.params.id)[0]
+  if (employee === undefined) res.status(404), res.send()
+  else res.send(employee)
 })
 
 app.put('/employees/:id', function(req, res) {
-  let i = data.employees.findIndex(e=>e.id==req.params.id)
-
-  for(key of Object.keys(req.body)) {
-    data.employees[i][key] = req.body[key] 
+  let i = data.employees.findIndex(e=>e?.id==req.params.id)
+  let employee = data.employees[i]
+  if(employee === undefined) {
+    res.status(404)
+    res.send("Employee not found")
+  } else if (req.body.id !== undefined) {
+    res.status(400)
+    res.send("Cannot update primary id")
+  } else { 
+    console.log(`User ${employee.id} updated:`)
+    for(key of Object.keys(req.body)) {
+      console.log(`  ${key}: ${employee[key]} => ${req.body[key]}`)
+      employee[key] = req.body[key] 
+    }
+  
+    res.send(employee)
   }
-
-  res.send(data.employees[i])
 })
 
-app.delete('employees/:id', function(req, res) {
-  let i = data.employees.findIndex(e=>e.id==req.params.id)
+app.delete('/employees/:id', function(req, res) {
+  let i = data.employees.findIndex(e => e?.id==req.params.id)
+  if(i === -1) {
+    res.status(404)
+    res.send("Employee not found")
+    return
+  }
+
+  let id = data.employees[i].id
   delete data.employees[i]
+  data.employees = data.employees.filter(e => e !== null)
+  console.log(`User ${id} deleted.`)
   res.send('OK')
 })
 
@@ -58,7 +82,7 @@ app.get('/clients', function(req, res) {
 })
 
 app.post('/clients', function(req, res) {
-  if (req.body.name) {
+  if (req.body.name !== undefined) {
     let client = {
       name: req.body.name,
       url: req.body.url,
@@ -66,6 +90,11 @@ app.post('/clients', function(req, res) {
     }
   
     data.clients.push(client)
+    console.log(`Client ${client.id} created:`)
+    for(key of Object.keys(req.body)) {
+      if (key.toLowerCase() === "id") continue
+      console.log(`  ${key}: ${client[key]}`)
+    }
     res.send(client)
   } else {
     res.status(400).send('Missing client name')
@@ -73,22 +102,44 @@ app.post('/clients', function(req, res) {
 })
 
 app.get('/clients/:id', function(req, res) {
-  res.send(data.clients.filter(e=>e.id == req.params.id)[0])
+  let client = data.clients.filter(e=>e?.id == req.params.id)[0] 
+  if (client === undefined) res.status(404), res.send()
+  else res.send(client)
 })
 
 app.put('/clients/:id', function(req, res) {
-  let i = data.clients.findIndex(e=>e.id==req.params.id)
+  let i = data.clients.findIndex(e => e?.id==req.params.id)
+  let client = data.clients[i]
 
-  for(key of Object.keys(req.body)) {
-    data.clients[i][key] = req.body[key] 
-  }
-
-  res.send(data.clients[i])
+  if(client === undefined) {
+    res.status(404)
+    res.send("Client not found")
+  } else if (req.body.id !== undefined){
+    res.status(400)
+    res.send("Cannot update primary id")
+  } else {
+    console.log(`Client ${client.id} updated:`)
+    for(key of Object.keys(req.body)) {
+      console.log(`  ${key}: ${client[key]} => ${req.body[key]}`)
+      client[key] = req.body[key] 
+    }
+  
+    res.send(client)
+  } 
 })
 
-app.delete('clients/:id', function(req, res) {
-  let i = data.clients.findIndex(e=>e.id==req.params.id)
+app.delete('/clients/:id', function(req, res) {
+  let i = data.clients.findIndex(e=>e?.id==req.params.id)
+  if(i === -1) {
+    res.status(404)
+    res.send("Client not found")
+    return
+  }
+  
+  let id = data.clients[i].id
   delete data.clients[i]
+  data.clients = data.clients.filter(c => c !== null)
+  console.log(`Client ${id} deleted.`)
   res.send('OK')
 })
 
@@ -103,39 +154,66 @@ app.get('/contracts', function(req, res) {
 })
 
 app.post('/contracts', function(req, res) {
-  if(req.body.clientId) {
+  let client = data.clients.filter(e=>e.id==req.body.clientId)[0]
+  if(client !== undefined) {
     let contract = {
       id: newId(),
-      clientId: req.body.clientId,
+      clientId: client.id,
       type: req.body.type,
       startDate: req.body.startDate,
       endDate: req.body.endDate,
       tech: req.body.tech
     }
     data.contracts.push(contract)
+    console.log(`Contract ${contract.id} created:`)
+    for(key of Object.keys(req.body)) {
+      if (key.toLowerCase() === "id") continue
+      console.log(`  ${key}: ${contract[key]}`)
+    }
     res.send(contract)
   } else {
-    res.status(400).send('Missing client id')
+    res.status(400).send('Missing or invalid client id')
   }
+})
+
+app.get('/contracts/:id', function(req, res) {
+  let contract = data.contracts.filter(e=>e?.id == req.params.id)[0]
+  if (contract === undefined) res.status(404), res.send()
+  else res.send(contract)
 })
 
 app.put('/contracts/:id', function(req, res) {
   let i = data.contracts.findIndex(e=>e.id==req.params.id)
+  let contract = data.contracts[i]
+  if(contract === undefined) {
+    res.status(404)
+    res.send("Contract not found")
+  } else if(req.body.id !== undefined){
+    res.status(400)
+    res.send("Cannot update primary id")
+  } else {
+    console.log(`Contract ${contract.id} updated:`)
+    for(key of Object.keys(req.body)) {
+      console.log(`  ${key}: ${contract[key]} => ${req.body[key]}`)
+      contract[key] = req.body[key] 
+    }
 
-  for(key of Object.keys(req.body)) {
-    data.contracts[i][key] = req.body[key] 
+    res.send(contract)
+  }   
+})
+
+app.delete('/contracts/:id', function(req, res) {
+  let i = data.contracts.findIndex(e=>e?.id==req.params.id)
+  if(i === -1) {
+    res.status(404)
+    res.send("Contract not found")
+    return
   }
 
-  res.send(data.contracts[i])
-})
-
-app.get('/contracts/:id', function(req, res) {
-  res.send(data.contracts.filter(e=>e.id == req.params.id)[0])
-})
-
-app.delete('contracts/:id', function(req, res) {
-  let i = data.contracts.findIndex(e=>e.id==req.params.id)
+  let id = data.contracts[i].id
   delete data.contracts[i]
+  data.contracts =  data.contracts.filter(c => c !== null)
+  console.log(`Contract ${id} deleted.`)
   res.send('OK')
 })
 
@@ -153,45 +231,76 @@ app.get('/history', function(req, res) {
 })
 
 app.post('/history', function(req, res) {
-  if(req.body.clientId !== undefined) {
-    let client = data.clients.filter(e=>e.id==req.body.clientId)[0]
-    let employee = data.employees.filter(e=>e.id==req.body.employeeId)[0]
+  let client = data.clients.filter(e=>e.id==req.body.clientId)[0]
+  let employee = data.employees.filter(e=>e.id==req.body.employeeId)[0]
+  let contract = data.contracts.filter(e=>e.id==req.body.contractId)[0]
+  if ( 
+    client !== undefined &&
+    employee !== undefined &&
+    contract !== undefined
+  ){
     let history = {
       id: newId(),
       clientId: client.id,
       clientName: client.name,
-      contractId: req.body.contractId,
+      contractId: contract.id,
       employeeId: employee.id,
       employeeName: employee.name,
       role: req.body.role
     }
     data.history.push(history)
+    console.log(`History ${history.id} created:`)
+    for(key of Object.keys(req.body)) {
+      if (key.toLowerCase() === "id") continue
+      console.log(`  ${key}: ${history[key]}`)
+    }
     res.send(history)
   } else {
-    res.status(400).send('Missing client id')
+    res.status(400).send('Missing or invalid client, contract, or employee id')
   }
-})
-
-app.put('/history/:id', function(req, res) {
-  let i = data.history.findIndex(e=>e.id==req.params.id)
-
-  for(key of Object.keys(req.body)) {
-    data.history[i][key] = req.body[key] 
-  }
-
-  res.send(data.history[i])
 })
 
 app.get('/history/:id', function(req, res) {
-  res.send(data.history.filter(e=>e.id == req.params.id)[0])
+  let history = data.history.filter(e=>e.id == req.params.id)[0]
+  if (history === undefined) res.status(404), res.send()
+  else res.send(history)
 })
 
-app.delete('history/:id', function(req, res) {
-  let i = data.history.findIndex(e=>e.id==req.params.id)
+app.put('/history/:id', function(req, res) {
+  let i = data.history.findIndex(e=>e?.id==req.params.id)
+  let history = data.history[i]
+  if(history === undefined) {
+    res.status(404)
+    res.send("History not found")
+  } else if(req.body.id !== undefined){
+    res.status(400)
+    res.send("Cannot update primary id")
+  } else {
+    console.log(`History ${history.id} updated:`)
+    for(key of Object.keys(req.body)) {
+      console.log(`  ${key}: ${history[key]} => ${req.body[key]}`)
+      data.history[i][key] = req.body[key] 
+    }
+  
+    res.send(data.history[i])
+  } 
+})
+
+
+app.delete('/history/:id', function(req, res) {
+  let i = data.history.findIndex(e=>e?.id==req.params.id)
+  if(i === -1) {
+    res.status(404)
+    res.send("History not found")
+    return
+  }
+
+  let id = data.history[i].id
   delete data.history[i]
+  data.history = data.history.filter(h => h !== null)
+  console.log(`History ${id} deleted.`)
   res.send('OK')
 })
-
 
 app.listen(port, host);
 console.log(`Server started at http://${host}:${port}`);
